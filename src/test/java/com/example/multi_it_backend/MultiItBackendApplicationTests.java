0e04d506422feb2858c098e6e5ebccb63aefb85e
ParkByeongsik <heaves06181223@gmail.com>
2024-06-10 23:10:12 +0900

    first job

---
+++ src/test/java/com/example/multi_it_backend/MultiItBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multi_it_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiItBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
