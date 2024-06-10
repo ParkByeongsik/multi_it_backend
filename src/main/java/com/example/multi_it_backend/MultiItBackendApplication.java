fe0963e46eb7e1e001d48b4eeccdb90d1f4192a4
ParkByeongsik <heaves06181223@gmail.com>
2024-06-10 23:08:23 +0900

    first job

---
+++ src/main/java/com/example/multi_it_backend/MultiItBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.multi_it_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiItBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiItBackendApplication.class, args);
+	}
+
+}
